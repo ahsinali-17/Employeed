e32019284278289f3cc07c540aa8b65707655e8d
ahsinali-17 <aajoker17@gmail.com>
2024-12-04 16:38:08 +0500

    .

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,33 +1,19 @@
 import { useUser } from '@clerk/clerk-react'
 import React from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
-import { useState,useEffect } from 'react'
 
 const ProtectedRoute = ({children}) => {
     const{user,isSignedIn,isLoaded}=useUser()
     const {pathname} = useLocation()  //get the current path after the domain name
-    const [redirect,setRedirect] = useState(false)
+    if(isLoaded && !isSignedIn && isSignedIn!==undefined){
+      
+     return <Navigate to='/?sign-in=true'/>
+    }
 
-    useEffect(() => {
-      if (isLoaded && !isSignedIn) {
-        // Delay redirect by a short duration to allow for state update.
-        const timeout = setTimeout(() => {
-          setRedirect(true);
-        }, 200); // Adjust delay as needed.
-  
-        return () => clearTimeout(timeout);
-      }
-    }, [isLoaded, isSignedIn]);
-  
-    if (redirect) {
-      return <Navigate to="/?sign-in=true" />;
+    //check onboarding status
+    if(user !== undefined && !user?.unsafeMetadata?.role && pathname !== '/onboarding'){
+      return <Navigate to = '/onboarding'/>
     }
-  
-    // Check onboarding status
-    if (user!==undefined && !user?.unsafeMetadata?.role && pathname !== '/onboarding') {
-      return <Navigate to="/onboarding" />;
-    }
-    
   return children
 
 }
